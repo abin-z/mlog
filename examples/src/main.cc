4084ab435cc6597cc595fa2183689518c7b3bd49
abin <abin.z@outlook.com>
2025-10-28 17:15:01 +0800

    test: 添加更多的测试用例

--- examples/src/main.cc
+++ examples/src/main.cc
@@ -18,6 +18,15 @@ int main()
     std::system("chcp 65001");  // 设置控制台为 UTF-8 编码
 #endif
     auto logger = LogManager::getLogger("my_logger");
+    logger->set_pattern("[%Y-%m-%d %H:%M:%S] [%l] [%n] %v");
+    logger->sinks()[0]->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] %v"); // 注意logger和sink的pattern是分开的, logger会覆盖sink的pattern
+    
+    
+    auto logger2 = LogManager::getLogger("logger2");
+    logger2->set_level(spdlog::level::trace); // logger2的日志级别设置为 trace, 这个会优先过滤
+    logger2->sinks()[0]->set_level(spdlog::level::level_enum::trace);  // 设置 logger2 的文件 sink 日志级别为 trace
+    logger2->sinks()[1]->set_level(spdlog::level::info);  // 设置 logger2 的文件 sink 日志级别为 info
+
     // LogManager::setStdoutGlobalLevel(spdlog::level::level_enum::info);  // 设置控制台日志级别为 info
     // LogManager::setFileGlobalLevel(spdlog::level::level_enum::warn);  // 设置文件日志级别为 warn
 
@@ -28,10 +37,18 @@ int main()
     logger->warn("这是一条 warn 日志");    // 会输出
     logger->error("这是一条 error 日志");  // 会输出
 
+
+    logger2->trace("这是一条 trace 日志");  // 不会输出，因为 level = info
+    logger2->debug("这是一条 debug 日志");  // 不会输出
+    logger2->info("这是一条 info 日志");    // 会输出
+    logger2->warn("这是一条 warn 日志");    // 会输出
+    logger2->error("这是一条 error 日志");  // 会输出
+
     // 模拟多条日志输出
     for (int i = 0; i < 10; ++i)
     {
       logger->info("日志测试条目 #{}", i);
+      logger2->info("日志测试条目 #{}", i);
       std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
